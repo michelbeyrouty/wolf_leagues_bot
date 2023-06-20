4b9882d5788413e8dabab2eb56961171e0751490
michelbeyrouty <michelbeyrouty@gmail.com>
2023-06-20 16:28:37 -0400

    Add new commands 🪮

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ client.on('ready', () => {
     console.log('Logged in as ' + client.user.tag);
 });
 
-client.on('messageCreate', (msg) => {
+client.on('messageCreate', async (msg) => {
     if (msg.author.bot) return;
     if (!msg.content.startsWith(prefix)) return;
 
@@ -19,10 +19,30 @@ client.on('messageCreate', (msg) => {
 
     if (userCmd === commands.getName) {
         msg.reply("Your discord username is " + msg.author.username);
+    } else if (userCmd === commands.tellJoke) {
+        msg.channel.send('HTML bla bla bla');
+    } else if (userCmd === commands.sad) {
+        msg.reply("Don't be sad! This is not the end of the road");
+    } else if (userCmd === commands.lastMsgs) {
+        const reply = await getLastMsgs(msg);
+        msg.reply(reply.join("\n"));
     } else {
         msg.reply('I do not understand your command');
     }
 });
+
+const getLastMsgs = async (msg) => {
+    // fetching the last 10 messages
+    const res = await msg.channel.messages.fetch({ limit: 10 });
+
+    const lastTenMsgs = res.map((message) => {
+        return message.content;
+    });
+
+    console.log(lastTenMsgs)
+
+    return lastTenMsgs;
+};
 
 client.login(config.DISCORD_TOKEN);
 
